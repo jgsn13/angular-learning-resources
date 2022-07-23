e63e98abbe7beb0adadccb9847dbf88aa69c6046
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-07-23 11:42:50 -0300

    feat: creating a component with angular CLI

--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent],
+  declarations: [AppComponent, UserComponent, UsersComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
